55220bd9b9bd8ea1832b551061702754321f34bc
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-05-22 23:38:24 +0700

    Menambahkan class FeaturePlantCard

--- lib/screen/home/components/featured_plants.dart
+++ lib/screen/home/components/featured_plants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plantapp/constraints.dart';
 
 class FeaturedPlants extends StatelessWidget {
   const FeaturedPlants({super.key});
@@ -25,3 +26,30 @@ class FeaturedPlants extends StatelessWidget {
   static void _doNothing() {}
 }
 
+class FeaturePlantCard extends StatelessWidget {
+  final String image;
+  final VoidCallback press;
+
+  const FeaturePlantCard({super.key, required this.image, required this.press});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin: const EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding / 2,
+          bottom: kDefaultPadding / 2,
+        ),
+        width: size.width * 0.8,
+        height: 185,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(fit: BoxFit.cover, image: AssetImage(image)),
+        ),
+      ),
+    );
+  }
+}
