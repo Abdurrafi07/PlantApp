13bc50c3f1cad2127c633ba740b6f1503cec43be
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-05-22 23:49:22 +0700

    Menambahkan header dan section Plants

--- lib/screen/home/components/body.dart
+++ lib/screen/home/components/body.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:plantapp/constraints.dart';
+import 'package:plantapp/screen/home/components/featured_plants.dart';
+import 'package:plantapp/screen/home/components/header_with_searchbox.dart';
+import 'package:plantapp/screen/home/components/recomend_plants.dart';
+import 'package:plantapp/screen/home/components/title_with_more_btn.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -8,6 +13,19 @@ class Body extends StatelessWidget {
     final Size size = MediaQuery.of(context).size;
 
     return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          HeaderWithSearchbox(size: size),
+          TitleWithMoreBtn(title: "Recomended", press: _doNothing),
+          RecomendPlants(),
+          TitleWithMoreBtn(title: "Feature Plants", press: _doNothing),
+          FeaturedPlants(),
+          SizedBox(height: kDefaultPadding),
+        ],
+      ),
     );
   }
+
+  static void _doNothing() {}
 }
