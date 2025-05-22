67d99ae909dec361756b58b2f9e4f9942019027b
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-05-22 22:43:49 +0700

    Menambahkan Konstruktor dan Variable TitleWithMoreBtn

---
+++ lib/screen/home/components/title_with_more_btn.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TitleWithMoreBtn extends StatelessWidget {
+  final String title;
+  final VoidCallback press;
+
+  const TitleWithMoreBtn({required this.title, required this.press, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
