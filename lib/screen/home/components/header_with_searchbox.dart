21a0c9832486edb525d8ac8e7f8c46feb3655003
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-05-22 22:21:04 +0700

    Membuat Header Container dan Bacground

--- lib/screen/home/components/header_with_searchbox.dart
+++ lib/screen/home/components/header_with_searchbox.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:plantapp/constraints.dart';
 
 class HeaderWithSearchbox extends StatelessWidget {
-  const HeaderWithSearchbox({super.key});
+  final Size size;
+  const HeaderWithSearchbox({super.key, required this.size});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      margin: const EdgeInsets.only(bottom: kDefaultPadding * 2.5),
+      height: size.height * 0.25,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+              left: kDefaultPadding,
+              right: kDefaultPadding,
+              bottom: 36 + kDefaultPadding + 30,
+            ),
+            height: size.height * 0.25 - 27,
+            decoration: const BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(36),
+                bottomRight: Radius.circular(36),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
