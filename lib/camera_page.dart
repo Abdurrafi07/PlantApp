d520efac6672c3c9173839c7469fdce66969d97a
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-05-23 02:29:03 +0700

    Inisialisasi kamera & Aktifkan mode fullscreen

---
+++ lib/camera_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
+import 'package:camera/camera.dart';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  late List<CameraDescription> _cameras;
+  CameraController? _controller;
+  int _selectedCameraIdx = 0;
+  FlashMode _flashMode = FlashMode.off;
+  double _zoom = 1.0;
+  double _minZoom = 1.0;
+  double _maxZoom = 1.0;
+  bool _isZoomSupported = false;
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    _initialCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
