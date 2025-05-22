7a9a98b4f2421513cbc1d3a4cfc331264c6ae461
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-05-22 22:05:28 +0700

    Membuat StatelessWidget HeaderWithSearchbox

---
+++ lib/screen/home/components/header_with_searchbox.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HeaderWithSearchbox extends StatelessWidget {
+  const HeaderWithSearchbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
