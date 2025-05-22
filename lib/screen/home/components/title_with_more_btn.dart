08a3f18fe783b30cfeaa160d493182415babde93
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-05-22 23:03:51 +0700

    Menambahkan Class TitleWithCustomUnderline

--- lib/screen/home/components/title_with_more_btn.dart
+++ lib/screen/home/components/title_with_more_btn.dart
@@ -35,3 +35,42 @@ class TitleWithMoreBtn extends StatelessWidget {
   }
 }
 
+class TitleWithCustomUnderline extends StatelessWidget {
+  final String text;
+
+  const TitleWithCustomUnderline({
+    required this.text,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 24,
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text(
+              text,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: const EdgeInsets.only(right: kDefaultPadding / 4),
+              height: 7,
+              color: kPrimaryColor.withOpacity(0.2),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
