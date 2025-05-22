2bf74e877f630f8de1f0e587068fb2c69e5306d7
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-05-23 02:20:29 +0700

    Menambahkan posisi alamat sekarang

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -185,6 +185,25 @@ class _MapPageState extends State<MapPage> {
               markers: _pickedMarker != null ? {_pickedMarker!} : {},
               onTap: _onTap,
             ),
+            Positioned(
+              top: 25,
+              left: 50,
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 4,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: Text(_currentAddress ?? 'Kosong'),
+              ),
+            ),
           ],
         ),
       ),
