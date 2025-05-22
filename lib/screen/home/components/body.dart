3522357b5988edf3037cd34aa6335b5f1b5f0015
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-05-22 22:05:02 +0700

    Membuat StatelessWidget Body

---
+++ lib/screen/home/components/body.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+    );
+  }
+}
